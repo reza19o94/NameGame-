569029c3d3e6efdb627a9fccced4f4c402d0acfa
reza19o94 <reza.it.94@gmail.com>
2025-09-22 08:30:30 +0330

    Create Constants.java

---
+++ app/app/src/main/ app/src/main/res/values/app/src/main/java/com/example/namegame/Constants.java
@@ -0,0 +1,18 @@
+package com.example.namegame;
+
+public class Constants {
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    public static final int GAME_START = 10;
+    public static final int GAME_TURN = 11;
+    public static final int GAME_ANSWER = 12;
+    public static final int GAME_SCORE = 13;
+    public static final int GAME_END = 14;
+}
